d66d21a51dac52adf576b3559d569b024ac1d342
Sobothty <supercrisz24@gmail.com>
2025-06-05 10:08:11 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,20 @@
+import { Footer } from './src/components/Footer.js';
+import { Navbar } from './src/components/Navbar.js';
+
+// adding navbar components
+document.addEventListener('DOMContentLoaded', () => {
+    const header = document.getElementById('header');
+    if (header) {
+        header.innerHTML = '';
+        header.appendChild(Navbar());
+    }
+});
+
+// adding footer components
+document.addEventListener('DOMContentLoaded', () =>{
+    const footer = document.getElementById('footer');
+    if (footer) {
+        header.innerHTML = '';
+        footer.appendChild(Footer());
+    }
+});
